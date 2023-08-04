425cebae5f4d5cd582bd90108233ec8232a945aa
WesleyBanagouro <wesley.banagouro@outlook.com>
2023-08-04 15:38:26 -0300

    exercicio 60

--- python/aula 14/ex060.py
+++ python/aula 14/ex060.py
@@ -3,12 +3,14 @@ o seu fatorial.'''
 
 num = int(input('Digite um número: '))
 fatoriais = []
+resultado = 1
 
-fat = 1
 i = 1
 
-while i < num:
+while i <= num:
+    resultado *= i
     fatoriais.append(i)
     i += 1
 
-print('Voce digitou o número {}, ou seja, seu fatorial são os números {}. O resultado é igual a'.format(num, for i in fatoriais: print(i)))
+fatoriais_str = ', '.join(map(str, fatoriais))
+print('Você digitou o número {}, ou seja, os fatoriais são os números {}. O resultado é igual a {}.'.format(num, fatoriais_str, resultado))

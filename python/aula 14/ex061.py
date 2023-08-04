7d9364c8a520b465d4a5d7d4e706d6abafffec3e
WesleyBanagouro <wesley.banagouro@outlook.com>
2023-08-04 15:54:20 -0300

    exercicio 61

--- python/aula 14/ex061.py
+++ python/aula 14/ex061.py
@@ -1,3 +1,12 @@
 '''Refaça o desafio 51, lendo o primeiro termo e a razão
 de uma PA, mostrando os 10 primeiros termos da progressão
 usando a estrutura while.'''
+
+razao = int(input('Digite a razão da PA: '))
+primeiro = int(input('Digite o primeiro valor da PA: '))
+numero = 1  # Variável para contar o número de termos exibidos
+
+while numero <= 10:  # Vamos exibir os 10 primeiros termos da PA
+    print(primeiro, end=' ')
+    primeiro += razao
+    numero += 1

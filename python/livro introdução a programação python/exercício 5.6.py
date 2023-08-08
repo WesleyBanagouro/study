878f64c110c65a978ed0036939128e44f0a26ddb
WesleyBanagouro <wesley.banagouro@outlook.com>
2023-08-08 15:49:05 -0300

    exercício 5.6

---
+++ python/livro introdução a programação python/exercício 5.6.py
@@ -0,0 +1,16 @@
+'''n = int(input("Tabuada de:"))
+x = 1
+while x <= 10:
+print(n*x)
+ x=x*1
+
+Exercício 5.6 Altere o programa anterior para exibir os resultados no mesmo formato de uma tabuada: 2x1 = 2, 2x2=4, ...'''
+
+n = int(input("Tabuada de:"))
+x = 1
+
+while x <= 10:
+    resultado = n * x
+    print("-" * 30)
+    print("{} x {} = {}".format(n, x, resultado))
+    x += 1

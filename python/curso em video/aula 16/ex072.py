f6e2b15d2ee82c9f4804e50569a694ce96d0b796
WesleyBanagouro <wesley.banagouro@outlook.com>
2023-08-21 18:46:04 -0300

    exercício 72

--- python/curso em video/aula 16/ex072.py
+++ python/curso em video/aula 16/ex072.py
@@ -4,4 +4,10 @@ com uma contagem por extenso, de 0 até 20.
 Seu programa deverá ler um número pelo teclado (entre 0 e 20) e
 mostra-lo por extenso.'''
 
-contagem = ('Zero', 'Um', 'Dois', 'Tres', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove' , 'Vinte')
+contagem = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove' , 'vinte')
+
+numero = int(input('Digite um número de 0 a 20: '))
+
+while numero < 0 or numero > 20:
+    numero = int(input('Número inválido. Digite um número de 0 a 20: '))
+print(f'Você digitou o número {contagem[numero]}')

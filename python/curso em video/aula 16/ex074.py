2aa57fe75870f6865d0567906007229da1156170
WesleyBanagouro <wesley.banagouro@outlook.com>
2023-08-21 19:36:15 -0300

    exercício 74

--- python/curso em video/aula 16/ex074.py
+++ python/curso em video/aula 16/ex074.py
@@ -3,3 +3,17 @@ e colocar em uma tupla.
 
 Depois disso, mostre a listagem de números gerados e tambem
 indique o menor e o maior valor que estão na tupla.'''
+
+import random
+
+t = ()
+
+while len(t) <= 4:
+    n = (random.randint(1, 10))
+    t = t + (n,)
+
+print('Os números sorteados foram:', end=' ')
+for n in t:
+    print(n, end=' ')
+print(f"\nO maior valor na tupla é: {max(t)}")
+print(f"O menor valor na tupla é: {min(t)}")

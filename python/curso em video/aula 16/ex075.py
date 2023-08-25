bd7dbb65252ffd106466ef01dd37648025c6d39e
WesleyBanagouro <wesley.banagouro@outlook.com>
2023-08-25 00:18:27 -0300

    terminando exercício 75

--- python/curso em video/aula 16/ex075.py
+++ python/curso em video/aula 16/ex075.py
@@ -8,9 +8,9 @@ B-) Em que posição foi digitado o primeiro valor 3.
 C-) Quais foram os números pares.'''
 
 valores = ()
-cont_9 = 0
 cont = 0
 posicao_3 = 0
+pares = ()
 
 while cont < 4:
     valor_entrada = int(input('Digite um valor: '))
@@ -18,10 +18,13 @@ while cont < 4:
     cont += 1
 
 for valor in valores:
-    if valor == 9:
-        cont_9 += 1
-    elif valor == 3:
-    
+    if valor % 2 == 0:
+        pares += (valor,)
 
+cont_9 = valores.count(9)
+posicao_3 = valores.index(3)
 
 print(valores)
+print(f'Na tupla digitada, há {cont_9} números 9.')
+print(f'Na tupla digitada, o número 3 encontra - se na {posicao_3 + 1}ª posição.')
+print(f'Os números pares são {pares}.')

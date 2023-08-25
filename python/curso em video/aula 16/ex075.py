b296db308fe91bbf1d6aae794a598249a741beff
WesleyBanagouro <wesley.banagouro@outlook.com>
2023-08-24 23:18:07 -0300

    exercicio 75

--- python/curso em video/aula 16/ex075.py
+++ python/curso em video/aula 16/ex075.py
@@ -6,3 +6,22 @@ A-) Quantas vezes apareceu o valor 9.
 B-) Em que posição foi digitado o primeiro valor 3.
 
 C-) Quais foram os números pares.'''
+
+valores = ()
+cont_9 = 0
+cont = 0
+posicao_3 = 0
+
+while cont < 4:
+    valor_entrada = int(input('Digite um valor: '))
+    valores += (valor_entrada,)
+    cont += 1
+
+for valor in valores:
+    if valor == 9:
+        cont_9 += 1
+    elif valor == 3:
+    
+
+
+print(valores)

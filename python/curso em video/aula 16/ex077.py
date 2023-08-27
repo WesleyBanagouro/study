39e04b73e19158c6ad08e0a4635a98de9a5b3c63
WesleyBanagouro <wesley.banagouro@outlook.com>
2023-08-27 20:27:23 -0300

    exercicio 77

--- python/curso em video/aula 16/ex077.py
+++ python/curso em video/aula 16/ex077.py
@@ -1,3 +1,16 @@
 '''Crie um programa que tenha uma tupla com varias palavras 
 (não usar acentos). Depois disso, voce deve mostrar, para cada palavra
 quais são suas vogais.'''
+
+palavras = ('hamburger', 'queijo', 'batata', 'refrigerante')
+vogais = 'aeiouAEIOU'
+
+for palavra in palavras:
+    for letra in palavra:
+        vogais_palavra = []
+        if letra in vogais:
+            vogais_palavra.append(letra)
+print(vogais_palavra)
+    
+    
+    

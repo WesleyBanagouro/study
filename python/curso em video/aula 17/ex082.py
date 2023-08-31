92033b08bce6528a60b4c17ba1bfe391d197caaa
Wesley Banagouro <135990090+WesleyBanagouro@users.noreply.github.com>
2023-08-31 13:48:31 -0300

    exercício 82

--- python/curso em video/aula 17/ex082.py
+++ python/curso em video/aula 17/ex082.py
@@ -5,3 +5,27 @@ Depois disso, crie duas listas extras que vão conter apenas os
 valores pares e os valores ímpares digitados, respectivamente.
 
 Ao final, mostre o conteudo das tres listas geradas.'''
+
+c = 0
+
+numeros = []
+pares = []
+impares = []
+
+while c != 'N':
+    numero = int(input('Digite um número: '))
+    numeros.append(numero)
+    if numero % 2 == 0:
+        pares.append(numero)
+    else:
+        impares.append(numero)
+    c = input('Quer continuar? (s/n)').upper()
+print(f'A lista de números digitados: {numeros}.')
+if len(pares) == 0:
+    print('Você não digitou pares.')
+else:
+    print(f'A lista de números pares digitados: {pares}.')
+if len(impares) == 0:
+    print('Você não digitou ímpares.')
+else:
+    print(f'A lista de números impares digitados: {impares}.')

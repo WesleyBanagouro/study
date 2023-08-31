e3efe2b5fa5a6d4988843d9b598f69ffbfdd83eb
Wesley Banagouro <135990090+WesleyBanagouro@users.noreply.github.com>
2023-08-31 11:31:10 -0300

    exercício 79

--- python/curso em video/aula 17/ex079.py
+++ python/curso em video/aula 17/ex079.py
@@ -2,3 +2,18 @@
 numéricos e cadastre-os em uma lista. Caso o número já exista la
 dentro, ele não será adicionado. No final, serão exibidos todos os
 valores unicos digitados em ordem crescente.'''
+
+valores = []
+c = input('Vamos começar?').upper()
+
+while c == 'S':
+    valor = int(input('Digite um valor: '))
+    if valor in valores:
+        print('Esse valor já foi digitado. Ele não será adicionado.')
+        c = input('Quer continuar? (s/n)').upper()
+    else:
+        valores.append(valor)
+        print('Valor adicionado com sucesso!')
+        c = input('Quer continuar? (s/n)').upper()
+print(valores)
+    

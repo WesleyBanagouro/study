404ae5b205638495abc7e7fed4cc47f3d3d2e771
Wesley Banagouro <135990090+WesleyBanagouro@users.noreply.github.com>
2023-08-31 13:54:59 -0300

    exercicio 83

--- python/curso em video/aula 17/ex083.py
+++ python/curso em video/aula 17/ex083.py
@@ -1,3 +1,20 @@
 '''Crie um programa onde o usuário digite uma expressão qualquer 
 que use parenteses. Seu aplicativo deverá analisar se a expressão 
 passada esta com os parenteses abertos e fechados na ordem correta.'''
+
+
+expressao = input('Digite a expressão: ')
+pilha = []
+for caractere in expressao:
+    if caractere == '(':
+        pilha.append('(')
+    elif caractere == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está válida!')
+else:
+    print('Sua expressão está errada!')

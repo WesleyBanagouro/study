18ddcd356846b2aadc00432a1a4993c0f99d410d
WesleyBanagouro <wesley.banagouro@outlook.com>
2023-09-01 22:20:31 -0300

    ex082

--- python/curso em video/aula 17/ex082.py
+++ python/curso em video/aula 17/ex082.py
@@ -4,7 +4,7 @@ lista.
 Depois disso, crie duas listas extras que vão conter apenas os 
 valores pares e os valores ímpares digitados, respectivamente.
 
-Ao final, mostre o conteudo das tres listas geradas.'''
+Ao final, mostre o conteudo das 3 listas geradas.'''
 
 c = 0
 
@@ -13,14 +13,14 @@ pares = []
 impares = []
 
 while c != 'N':
-    numero = int(input('Digite um número: '))
+    numero = int(input('Digite um número para separar entre pares e impares: '))
     numeros.append(numero)
     if numero % 2 == 0:
         pares.append(numero)
     else:
         impares.append(numero)
-    c = input('Quer continuar? (s/n)').upper()
-print(f'A lista de números digitados: {numeros}.')
+    c = input('Quer continuar digitando? (s/n)').upper()
+print(f'A lista completa de números digitados: {numeros}.')
 if len(pares) == 0:
     print('Você não digitou pares.')
 else:

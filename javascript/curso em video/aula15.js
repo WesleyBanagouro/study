dc084db88600ba7e67d5675c82440a56716eaccf
WesleyBanagouro <wesley.banagouro@outlook.com>
2023-09-17 23:44:34 -0300

    testando funções javascript

--- javascript/curso em video/aula15.js
+++ javascript/curso em video/aula15.js
@@ -1,9 +1,9 @@
 let num = [3, 6, 9, 10, 44, 34]
 
 for (let pos = 0; pos < num.length; pos++){
-    console.log(`Na posição ${pos} temos o número ${num[pos]}.`)
+    console.log(`Na posição ${pos} temos os números ${num[pos]}.`)
 }
 
 for (let pos in num) {
-    console.log(`Na posição ${pos} temos o número ${num[pos]}.`)
+    console.log(`Na posição ${pos} temos os números ${num[pos]}.`)
 }
